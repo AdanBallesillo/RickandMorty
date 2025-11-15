7518c88ceec0b26ccdd6f46c204abbc3d777043a
AdanBallesillo <abv24032004@gmail.com>
2025-11-14 20:12:34 -0600

    correccion de ruta

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RickController;
 
-Route::get('/rick', [RickController::class, 'index'])->name('rick.index');
+Route::get('/', [RickController::class, 'index'])->name('rick.index');
 Route::get('/rick/{id}', [RickController::class, 'show'])->name('rick.show');
 Route::get('/episode/{id}', [RickController::class, 'episode'])->name('episode.show');
